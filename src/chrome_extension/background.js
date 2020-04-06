e2cac6c603e9cd98dcde287925719608f0b2f598
jfrisks <jo1875fr-s@student.lu.se>
2020-04-06 20:56:48 +0200

    Extension - Added detection of likebutton click so background.js can execute response

--- src/chrome_extension/background.js
+++ src/chrome_extension/background.js
@@ -1,4 +1,7 @@
 'use strict';
+const likeAction = "likeButtonClicked"
+const unlikeAction = "unlikeButtonClicked"
+
 
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
@@ -19,9 +22,35 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 
+//Listens for if script-likebutton is fired
+chrome.runtime.onMessage.addListener(function(message, sender, reply){
+    if(!sender.tab) return reply("Message from non-content script")
+    else if(message.action === likeAction){
+        //likebutton - TODO stuff
+
+        //reply
+        const message = 'like button registered in extension ' + sender.tab.url
+        reply(message)
+    }
+    else if(message.action === unlikeAction){
+        //unlikebutton - TODO stuff
+
+        //reply
+        const message = 'unlike button registered in extension ' + sender.tab.url
+        reply(message)
+    }
+    else{
+        //TODO stuff
+
+        //reply
+        const message = 'could not understand message sent to extension'
+        reply(message)
+    }
+})
+
 /** 
  * TODOS:
- * - Add eventlistener in background.js when savedbutton is pressed and active
- * - Send message to extension that it has been pushed
+ * X Add eventlistener in background.js when savedbutton is pressed and active
+ * X Send message to extension that it has been pushed
  * - Read message and do stuff
 */
--- src/chrome_extension/contentScript.js
+++ src/chrome_extension/contentScript.js
@@ -1,13 +1,30 @@
 'use strict';
 
 console.log('RealEstateHistory - Load contentcript')
+const unlikeButtonClassName = "save-listing__button--saved"
+const likeAction = "likeButtonClicked"
+const unlikeAction = "unlikeButtonClicked"
 
 window.addEventListener("load", function(event) { 
     console.log('RealEstateHistory - Page loaded fully')
 
-    //do work
-    let likeButton = document.getElementsByClassName('property-gallery__button--first')[0];
-    likeButton.addEventListener('click', function(){
-        alert('hello, clicked button')
+
+    //WHEN LIKE/UNLIKE-BUTTON CLICKED
+    let likeUnlikeButton = document.getElementsByClassName('property-gallery__button--first')[0];
+    likeUnlikeButton.addEventListener('click', function(){
+        //check if liked or unliked
+        const isUnliked = likeUnlikeButton.children[0].classList.contains(unlikeButtonClassName)
+        let action = isUnliked ? unlikeAction : likeAction
+
+        const message = {
+            action
+        }
+        
+        //send message to extension
+        chrome.runtime.sendMessage(undefined, message, undefined, function(responseMessage){
+            //calback
+            console.log(responseMessage)
+        })
+
     }, false);
-  });
+});
