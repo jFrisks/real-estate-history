614db888420fd845c82fc0ad77dbf000c5b16d03
jfrisks <jo1875fr-s@student.lu.se>
2020-04-12 21:14:54 +0200

    Extension - Rendered dummy images and getting tabUrl

--- src/chrome_extension/popup.js
+++ src/chrome_extension/popup.js
@@ -1,4 +1,9 @@
 let changeColor = document.getElementById('changeColor');
+let gallery = document.getElementById('imageGallery');
+const links = ["https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", "https://images.unsplash.com/photo-1568634143420-dc5368d74e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"]
+
+renderImages(gallery, links);
+getTabUrl()
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -14,3 +19,33 @@ changeColor.onclick = function(element){
         );
     });
 };
+
+function renderImages(parent, links){
+    for(let i = 0; i < links.length; i++){
+        let img = document.createElement('IMG');
+        img.setAttribute("src", links[i]);
+        img.setAttribute("width", "300");
+        parent.appendChild(img);
+    }
+}
+
+function getTabUrl(){
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const tab = tabs[0]
+        const tabUrl = tab.url
+        
+        const listingPath = parseHemnetId(tabUrl)
+        console.log(listingPath)
+        
+        //get info about current id
+        chrome.storage.sync.get(listingPath, function(data) {
+            //TODO: Fix this
+            console.log(data)
+        });
+    })
+}
+
+function parseHemnetId(url){
+    //"","bostad", "listing-id"
+    return new URL(url).pathname.split('/')[2];
+}
