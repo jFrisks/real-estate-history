42a69f4115e7f390f4ff9a9499c68f9e7811832e
jfrisks <jo1875fr-s@student.lu.se>
2020-04-14 14:32:19 +0200

    Tests - Added test config and first test for working listing. Renamed old test-method-file to try

---
+++ src/tests/mainScraper.test.js
@@ -0,0 +1,49 @@
+const mainScraper = require('../server/mainScraper');
+const { generateIdFromUrl } = require('../server/utils')
+
+test('test multiple buyable listing links', async () => {
+    jest.setTimeout(40000);
+    const testListings = [
+        "https://www.hemnet.se/bostad/lagenhet-2rum-sodermalm-stockholms-kommun-slipgatan-12,-1,5-tr-16659036"
+    ]
+    const listings = await mainScraper.getListings(testListings);
+    const listingIDs = testListings.map((link, index) => generateIdFromUrl({url: link}))
+    //expect result to be {id1: {images:[], ...other}, id2: {images:[], ...other}
+
+    expect(listings).toBeDefined();
+    //expect(listings).toBe({"lagenhet-2rum-lilla-essingen-kungsholmen-stockholms-kommun-stralgatan-23,-4-tr-16759892": "hej"});
+    expect(listings).toHaveProperty(listingIDs[0])
+    const listing1Obj = listings[listingIDs[0]];
+    expect(listing1Obj.images).toBeDefined();
+    expect(listing1Obj.images[0]).toEqual(
+        expect.stringMatching(/http/)
+    );
+});
+
+
+// async function test(){
+//     //const result = await mainScraper.getListingsInfo(listingLinks, async (link) => await mainScraper.getPageListingInfo(link))
+//     //const result = await mainScraper.getListingsInfo(listingLinks, async (link) => await mainScraper.getPageListingImages(link))
+//     //const result = await mainScraper.getPageListingImages(listingLinks)
+//     //console.log('Result of test is', result)
+
+//     // const x = {"1": {"a": "hej"}, "2":{"a": "hej"}}
+//     // const y = {"1": {"b": "san"}, "2":{"b": "san"}}
+//     // console.log(mergeDeep(x, y))
+// }
+
+
+// function testBadLink(){
+//     const listings = mainScraper.getListings(["https://www.hemnet.se/bostad/lagenhet-2rum-lilla-essingen-kungsholmen-stockholms-kommun-stralgatan-23,-4-tr-16759"]);
+// }
+
+// function testSoldListing(){
+//     //test solf but old link
+
+//     //test sold on sold-link
+
+// }
+
+// function testTerminatedServer(){
+
+// }
