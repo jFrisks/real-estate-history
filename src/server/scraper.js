95a1875ffcb1e232011cffeb0407c45bd57db0aa
jfrisks <jo1875fr-s@student.lu.se>
2020-04-14 14:28:55 +0200

    Scraper - handled errors and then return {images: []}

--- src/server/scraper.js
+++ src/server/scraper.js
@@ -12,37 +12,42 @@ function scraper(){
     }
 
     const getListingImagesHemnet = async (url) => {
-        
-        const page = await browser.newPage();
-        await page.goto(url);
-        console.log('Headless scraper visited page ', url)
-        
-        //find amount of images
-        const moreImagesButton = await page.$('.property-gallery__button.qa-all-images-link.property-gallery__button.property-gallery__button--icon-right span')
-        const moreImagesButtonLabel = await page.evaluate(el => el.innerText, moreImagesButton);
-        const amountImages = parseInt(moreImagesButtonLabel.split(' ')[0])
-        
-        //click the same amount as images. This will load all slides.
-        console.log('Headless scraper starting to click next button ' + amountImages + ' times')
-        for (let i = 0; i < amountImages; i++) {
-            //click that amount on next-arrow
-            const nextImageButton = await page.$('.gallery-carousel__button.gallery-carousel__button--next')
-            await nextImageButton.click()
-        }
-        console.log('Headless scraper done clicking')
-        
-        //selectAllImages and save
-        const imageElements = await page.$$('.gallery-carousel__image-touchable img')
-        const imageLinks = []
-        
-        for(let i = 0; i < imageElements.length; i++){
-            const link = await page.evaluate(el => el.src, imageElements[i]);
-            imageLinks.push(link)
-        }
-        console.log('Headless scraper has collected all image src')
+        try{
+            const page = await browser.newPage();
+            await page.goto(url);
+            console.log('Headless scraper visited page ', url)
+            
+            //find amount of images
+            const moreImagesButton = await page.$('.property-gallery__button.qa-all-images-link.property-gallery__button.property-gallery__button--icon-right span')
+            const moreImagesButtonLabel = await page.evaluate(el => el.innerText, moreImagesButton);
+            const amountImages = parseInt(moreImagesButtonLabel.split(' ')[0])
+            
+            //click the same amount as images. This will load all slides.
+            console.log('Headless scraper starting to click next button ' + amountImages + ' times')
+            for (let i = 0; i < amountImages; i++) {
+                //click that amount on next-arrow
+                const nextImageButton = await page.$('.gallery-carousel__button.gallery-carousel__button--next')
+                await nextImageButton.click()
+            }
+            console.log('Headless scraper done clicking')
+            
+            //selectAllImages and save
+            const imageElements = await page.$$('.gallery-carousel__image-touchable img')
+            const imageLinks = []
+            
+            for(let i = 0; i < imageElements.length; i++){
+                const link = await page.evaluate(el => el.src, imageElements[i]);
+                imageLinks.push(link)
+            }
+            console.log('Headless scraper has collected all image src')
 
-        const imagesObject = {"images": imageLinks}
-        return imagesObject;
+            const imagesObject = {"images": imageLinks}
+            return imagesObject;
+        }catch(err){
+            console.error(err);
+            const imagesObject = {"images": []};
+            return imagesObject;
+        }
     };
 
     return {
