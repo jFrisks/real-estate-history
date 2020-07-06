aea3148b864cabb3dd33a3fd84e29494b4f6f687
jfrisks <jo1875fr-s@student.lu.se>
2020-07-06 17:44:06 +0200

    ContentScript - Added updating button if Listing saved in extension or not. Works after click or on reload

--- src/chrome_extension/contentScript.js
+++ src/chrome_extension/contentScript.js
@@ -6,8 +6,10 @@ const likeAction = "likeButtonClicked"
 const unlikeAction = "unlikeButtonClicked"
 const getSavedListingStateAction = "getSavedListingState";
 const hiSavedText = "HiSaved"
+const hiUnsavedText = "Not saved in Hi"
 const savedColor = "#2ef29a"
 const unsavedColor = "#f24f2e"
+const extensionButtonInfoClass = "hi-info-button"
 
 window.addEventListener("load", function (event) {
     console.log('RealEstateHistory - Page loaded fully')
@@ -23,20 +25,26 @@ window.addEventListener("load", function (event) {
             updateLikeButtonInfoText(likeUnlikeButton)
 
             //Click event listener
-            likeUnlikeButton.addEventListener('click', () => handleClickLikeUnlike(), false)
+            likeUnlikeButton.addEventListener('click', (event) => handleClickLikeUnlike(event), false)
             clearInterval(checkExist);
         }
     }, 100); // check every 100ms
-
-    function handleClickLikeUnlike() {
-        //check if liked or unliked
-        const isUnliked = likeUnlikeButton.children[0].classList.contains(unlikeButtonClassName)
-        let action = isUnliked ? unlikeAction : likeAction
-
-        sendMessageToBackgroundScript(action)
-
-    }
 });
+
+async function handleClickLikeUnlike(event) {
+    const likeUnlikeButton = event.currentTarget;
+    //const likeUnlikeButton = document.querySelector('.property-gallery__button--first');
+
+    //check if liked or unliked
+    const isUnliked = likeUnlikeButton.children[0].classList.contains(unlikeButtonClassName)
+    let clickAction = isUnliked ? unlikeAction : likeAction
+
+    //tell background script that button was clicked
+    await sendMessageToBackgroundScript(clickAction)
+
+    //Update buttonInfoText
+    updateLikeButtonInfoText(likeUnlikeButton)
+}
 
 function sendMessageToBackgroundScript(action){
     const message = {
@@ -51,7 +59,7 @@ function sendMessageToBackgroundScript(action){
             if(!responseMessage)
                 return reject(chrome.runtime.lastError)
             //console.log(responseMessage)
-            return resolve(responseMessage.isSaved)
+            return resolve(responseMessage)
         })
     })
 }
@@ -60,13 +68,14 @@ async function updateLikeButtonInfoText(likeButton){
     let isListingSavedInStorage = undefined;
     try{
         isListingSavedInStorage = await getIsListingSavedInStorage()
-        console.log(isListingSavedInStorage);
     }catch(err){
         console.error("An error appeared: " + err)
         isListingSavedInStorage = false;
     }
 
-
+    //remove old button infotext if any
+    const allExtensionInfoButtons = document.querySelectorAll(`.${extensionButtonInfoClass}`);
+    allExtensionInfoButtons.forEach(button => button.remove())
 
     //Shows correct button info depending on if listing is saved or not
     if(isListingSavedInStorage){
@@ -79,25 +88,25 @@ async function updateLikeButtonInfoText(likeButton){
     function addSavedInfoToButton(button){
         //Add saved info from extension to button
         let savedIcon = document.createElement("I")
-        savedIcon.setAttribute("class", "fa fa-cloud-download property-gallery__button-icon")
+        savedIcon.setAttribute("class", `fa fa-cloud-download property-gallery__button-icon ${extensionButtonInfoClass}`)
         savedIcon.setAttribute("style", `color: ${savedColor}; margin-left:8px;`)
         button.appendChild(savedIcon)
                     
         let savedText = document.createElement("span")
-        savedText.setAttribute("class", "property-gallery__button--label")
+        savedText.setAttribute("class", `property-gallery__button--label ${extensionButtonInfoClass}`)
         savedText.innerText = hiSavedText
         button.appendChild(savedText)
     }
     function addUnSavedInfoToButton(button){
         //Unsaved Text + Icon for hi data
         let unsavedIcon = document.createElement("I")
-        unsavedIcon.setAttribute("class", "fa fa-exclamation property-gallery__button-icon")
+        unsavedIcon.setAttribute("class", `fa fa-exclamation property-gallery__button-icon ${extensionButtonInfoClass}`)
         unsavedIcon.setAttribute("style", `color: ${unsavedColor}; margin-left:8px;`)
         button.appendChild(unsavedIcon)
 
         let unsavedText = document.createElement("span")
-        unsavedText.setAttribute("class", "property-gallery__button--label")
-        unsavedText.innerText = "Not saved in Hi"
+        unsavedText.setAttribute("class", `property-gallery__button--label ${extensionButtonInfoClass}`)
+        unsavedText.innerText = hiUnsavedText;
         button.appendChild(unsavedText)
     }
 }
@@ -106,16 +115,12 @@ async function getIsListingSavedInStorage(){
     //Message background and ask
     let isSaved = undefined;
     try{
-        isSaved = await sendMessageToBackgroundScript(getSavedListingStateAction)
+        const isSavedObject = await sendMessageToBackgroundScript(getSavedListingStateAction)
+        isSaved = isSavedObject.isSaved;
     }catch(err){
         console.error(err);
         isSaved = false;
     }
-    
-
     //get result from background 
-
     return isSaved
 }
-
-//TODO:  when button is clicked - update function for showing if saved
